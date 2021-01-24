0e1b5c3efd3fd2304dc52057fda20c505df5f347
surplusPolyCount <simonhrosenthal@gmail.com>
2021-01-23 19:32:30 -0800

    got webgl workin

--- js/test.js
+++ js/test.js
@@ -1,4 +1,7 @@
 
+/* INITIALIZATION CODE
+  (calls only on init)
+*/
 
 canvas = document.getElementsByTagName("canvas")[0];
 gl = canvas.getContext("webgl") || canvas.getContext("expiremental-webgl");
@@ -8,7 +11,7 @@ if(gl){
     console.log("huh, gotta find that shit");
 }
 
-//creates the shaders
+//creates the shader
 function createShader(gl, type, source) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -37,6 +40,7 @@ function createProgram(gl, vertexShader, fragmentShader) {
     gl.deleteProgram(program);
   }
 
+
 //get the text for the two different shader parts
 var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
 var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
@@ -47,3 +51,51 @@ var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
 //call shader
 var program = createProgram(gl, vertexShader, fragmentShader);
+
+var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+var positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); 
+
+var positions = [
+  0, 0, 
+  0, 0.5,
+  0.7, 0 
+];
+
+//copies positions array (once converted into strong type)
+//into the gl ARRAY_BUFFER (the gl.STATIC_DRAW just hints that the data in this array wont
+//be changed much)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+/*
+  RENDERING 
+  (called every frame)
+*/
+
+//webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+gl.viewport(0,0, gl.canvas.width, gl.canvas.height);
+
+//clear the canvas 
+gl.clearColor(0,0,0,0); 
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+//Tell it to use out program to execute 
+gl.useProgram(program);
+//turn on an attribute that we can use to submit data to the buffer
+gl.enableVertexAttribArray(positionAttributeLocation); 
+
+//specify how the data needs to be pulled out 
+//bind the position buffer 
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); 
+var size =2; //two components per iteration 
+var type = gl.FLOAT //data is 32bit floats
+var normalize = false; //don't normalize data 
+var stride = 0; //0 = moves forward size * sizeof(type) each iteration to get to the next position
+var offset = 0; //start at the beginning of the buffer 
+gl.vertexAttribPointer(
+  positionAttributeLocation, size, type, normalize, stride, offset);
+
+var primitiveType = gl.TRIANGLES; 
+var offset =0; 
+var count = 3; 
+gl.drawArrays(primitiveType, offset, count);
